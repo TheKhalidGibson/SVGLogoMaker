2b354a188508503a085c871b445f70c5654da371
Khalid G <kgibson4795@gmail.com>
2023-08-18 09:48:23 -0400

    added more to each shape class.. and also added to the parent/super class as well

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+import inquirer from "inquirer";
+import fs from "fs/promises";
 
 const fs = require("fs")
 let SVG = require("./lib/svg")
 
 let example = new SVG("AND", "green", "triangle", "yellow");
+
+let {fill, shape, logo} = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'fill',
+        message: "What color would you like the svg fill to be?",
+        choices: ['Green', 'Blue', 'Red', 'Yellow', 'Orange', 'Pink']
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: "What shape would you like the svg to be?",
+        choices: ['square', 'circle', 'triangle']
+      },
+      {
+        type: 'input',
+        name: 'logo',
+        message: "What logo would you like for the svg? Up to 3 characters",
+      },
+     ])
 
 
 
